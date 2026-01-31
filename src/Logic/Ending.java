d974784856304651a64538d3379b6a5779be27f4
kuba852cz <kubicek@spsejecna.cz>
2026-01-31 20:33:51 +0100

    Created Endings in gameData.json and class Ending Finalized TalkCommand

---
+++ src/Logic/Ending.java
@@ -0,0 +1,24 @@
+package Logic;
+
+public class Ending {
+    private String good;
+    private String bad;
+    private String preEnding;
+    private String prologue;
+
+    public Ending() {
+    }
+
+    public String getGood() {
+        return good;
+    }
+    public String getBad() {
+        return bad;
+    }
+    public String getPreEnding() {
+        return preEnding;
+    }
+    public String getPrologue() {
+        return prologue;
+    }
+}
