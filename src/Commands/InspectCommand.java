56190ce1783cdfe7d5103a2a77034c0dd938f10a
kuba852cz <kubicek@spsejecna.cz>
2026-01-31 17:39:01 +0100

    Finalized InspectCommand

--- src/Commands/InspectCommand.java
+++ src/Commands/InspectCommand.java
@@ -1,18 +1,49 @@
 package Commands;
 
+import Logic.Game;
+import Logic.GameData;
 import Models.Characters.Player;
 import Models.Item;
 
 public class InspectCommand implements Command {
     private Player player;
+    private GameData gameData;
 
-    public InspectCommand(Player player) {
+    public InspectCommand(Player player, GameData gameData) {
         this.player = player;
+        this.gameData = gameData;
     }
 
     @Override
     public String execute(String targetingName) {
-        return player.getCurrentRoom().getDescription();
+        if (targetingName.isEmpty()) {
+            String popis = "Jsi v: " + player.getCurrentRoom().getName() + "\n" +player.getCurrentRoom().getDescription();
+            if (!player.getCurrentRoom().getItems().isEmpty()) {
+                popis += "\n" + "Vidis tu: ";
+                for (String itemId : player.getCurrentRoom().getItems()){
+                    Item item = gameData.findItem(itemId);
+                    popis += item.getName() + ", ";
+                }
+                popis = popis.substring(0, popis.length() - 2);
+            }else{
+                popis += "Nic zajimaveho tady nevidis.";
+            }
+            return popis;
+        }
+
+        for (String addingItemID : player.getCurrentRoom().getItems()) {
+            Item item = gameData.findItem(addingItemID);
+            if (item.getName().equals(targetingName)) {
+                return item.getDescription();
+            }
+        }
+
+        for (Item item : player.getInventory()) {
+            if (item.getName().equals(targetingName)) {
+                return item.getDescription();
+            }
+        }
+        return "Nic zajimaveho se zde nenachazi.";
     }
 
     @Override
